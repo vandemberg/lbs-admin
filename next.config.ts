bfeeac7fcaeca71387c579a11a7beb08eec6fd1c
vandemberg lima <vandemberg.silva.lima@gmail.com>
2025-01-28 10:37:42 -0300

    Add cursos leaders in company to next.js config

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["github.com", "images.unsplash.com", "localhost"],
+    domains: [
+      "github.com",
+      "images.unsplash.com",
+      "localhost",
+      "cursos.leadersincompany.com",
+    ],
   },
 };
 
